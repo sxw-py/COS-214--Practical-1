c12a77cb429c51c690970c2f4279151a059644bb
ATS Mthemba <ashmthemba@gmail.com>
2026-07-29 03:05:55 +0200

    Partially implemented Transformation

---
+++ Transformation.cpp
@@ -0,0 +1,51 @@
+//Task 2 
+#include "Transformation.h"
+
+Transformation::Transformation(string name){
+    this->name = name;
+}
+
+string Transformation::getName(){
+    return name;
+}
+
+vector<string> DeduplicateStep::apply(vector<string> arr) {
+    if (arr.empty()){
+        return arr;
+    }
+
+    vector<string> trans{};
+    for (int i = 0; i < arr.size() - 1; ++i){
+        if (arr[i] != arr[i+1]){
+            trans.push_back(arr[i]);
+        }
+    }
+
+    if (arr.size() > 2){ //handles pushing of last element onto the vector 
+        if (arr[arr.size()-1] != arr[arr.size()-2]){
+            trans.push_back(arr[arr.size()-1]);
+        }
+    } else if (arr.size() == 2) {
+        trans.push_back(arr[0]);
+    }
+
+return trans;
+
+} 
+
+Transformation* DeduplicateStep::clone() const{
+    return new DeduplicateStep(*this);
+}
+
+
+vector<string> AggregateByRegionStep::apply(vector<string> arr){
+    string out = "COUNT=";
+    out += to_string(arr.capacity());
+    vector<string> trans{out};
+    return trans;
+}
+
+Transformation* AggregateByRegionStep::clone() const{
+    return new AggregateByRegionStep(*this);
+}
+
---
+++ Transformation.h
@@ -0,0 +1,43 @@
+#ifndef Transformation_h
+#define Transformation_h
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+using namespace std;
+
+class Transformation{
+    protected:
+        string name;
+    public:
+        Transformation(string);
+        virtual Transformation* clone() const = 0;
+        virtual vector<string> apply(vector<string> ) = 0;
+        string getName();
+        virtual ~Transformation();
+
+};
+
+class DeduplicateStep: public Transformation {
+    public:
+        using Transformation::Transformation; // FIX FOR: base class has parameterised constructor!
+                                              //Derived class won't be able to be instantiate without compilation error
+
+        Transformation* clone() const override; 
+        vector<string> apply(vector<string> ) override;
+};
+
+class AggregateByRegionStep: public Transformation {
+    public:
+        using Transformation::Transformation; //Same fix as dedup
+
+        Transformation* clone() const override; 
+        vector<string> apply(vector<string> ) override;
+};
+
+
+
+
+
+#endif
