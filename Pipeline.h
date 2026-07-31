370e6af6d84609b0e960f87a1a1bb44de3b14834
sxw-py <ashmthemba@gmail.com>
2026-07-31 09:39:10 +0200

    Updated Pipeline header

--- Pipeline.h
+++ Pipeline.h
@@ -6,6 +6,7 @@
 #include <map>
 #include "Task1/ConnectorFactory.h"
 #include "Transformation.h"
+//#include "Task4/RunCheckpoint.h"
 
 using namespace std;
 
@@ -16,6 +17,30 @@ class Pipeline{
         vector<Transformation*> steps;
         int stage;
         vector<string> records;
+        void connect();
+        virtual void extract() = 0;
+        void transform();
+        virtual void load() = 0;
+    public:
+        Pipeline(ConnectorFactory*);
+        void run();
+        void addStep(Transformation*);
+        //RunCheckpoint* createCheckpoint();
+        //void restore(RunCheckpoint*);
+        virtual ~Pipeline();
+    
+};
+
+class BatchPipeline : public Pipeline{
+    protected:
+        void extract() override;
+        void load() override;
+};
+
+class StreamingPipeline : public Pipeline{
+    protected:
+        void extract() override;
+        void load() override;
 };
 
 
